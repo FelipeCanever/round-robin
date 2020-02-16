e61abcc23e1ff6bbd23db748b5fb58f0f50f7078
Felipe Canever Fernandes <felipe.canever@outlook.com>
2020-02-15 22:08:25 -0300

    Implement quantum and number of processes prompts.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+using std::cin;
 using std::cout;
 
 auto main() -> int
 {
-	cout << "Hello, Round Robin!\n";
+	cout << "ROUND-ROBIN SCHEDULING" << "\n\n";
+
+	cout << "Quantum (ms): ";
+	auto quantum = int{};
+	cin >> quantum;
+	cout << "\n";
+
+	cout << "Number of processes: ";
+	auto n = int{};
+	cin >> n;
+	cout << "\n";
 }
